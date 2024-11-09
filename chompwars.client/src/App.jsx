3ee16878f864230091b3ccadae95ffea27134437
BojidarS <pichbg124@gmail.com>
2024-11-10 00:47:35 +0200

    Added Logo and Basic Layout

--- chompwars.client/src/App.jsx
+++ chompwars.client/src/App.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
+import Layout from './components/Layout';
 import { Button } from '@mui/material';
 
 function App() {
     return (
-        <div>
-            <h1>Welcome to ChopWars!</h1>
-            <Button variant="contained" color="primary">Play Now</Button>
-        </div>
+        <Layout>
+           <div>
+              <h1>Welcome to ChopWars!</h1>
+              <Button variant="contained" color="primary">Play Now</Button>
+            </div>
+        </Layout>
     );
 }
 
---
+++ chompwars.client/src/components/Layout.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Box, Container } from '@mui/material';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            {/* Header Section with Logo and Navigation */}
+           <AppBar position='sticky'>
+            <Toolbar>
+                {/* Logo and App Name */}
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1}}>
+                    <Link to='/' style={{ color: 'white', textDecoration: 'none' }}>
+                      <img src='/src/assets/logo.png' style={{ width: '60px', height: '60px', marginRight: '10px' }}></img>
+                    </Link>
+                    Chomp Wars
+                </Typography>
+
+                {/* Navigation Buttons */}
+                <Button color='inherit'>Home</Button>
+                <Button color='inherit'>Play</Button>
+                <Button color='inherit'>About</Button>
+                <Button color='inherit'>Login</Button>
+                <Button color='inherit'>Register</Button>
+            </Toolbar>
+           </AppBar>
+
+           {/* Main Content */}
+           <Container maxWidth='lg' sx={{ mt: 2 }}>
+            {children}
+           </Container>
+
+           {/* Footer*/}
+           <Box position='fixed' component='footer'
+           sx=
+           {{ textAlign: 'center', padding: '10px 0', backgroundColor: '#f1f1f1', marginTop: '20px', bottom: 5, width: '100%' }}>
+           <Typography variant="body2" color="textSecondary">
+             &copy; 2024 Chomp Wars - All rights reserved.
+           </Typography>
+           </Box>
+        </div>
+    );
+};
+
+export default Layout;
