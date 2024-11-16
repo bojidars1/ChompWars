8af2feaaee67e00e00012ef9874033d12192a1c7
BojidarS <pichbg124@gmail.com>
2024-11-16 21:52:17 +0200

    Added Game Camera feature

--- chompwars.client/src/components/GameCanvas.jsx
+++ chompwars.client/src/components/GameCanvas.jsx
@@ -3,6 +3,11 @@ import React, { useEffect, useRef } from 'react';
 const GameCanvas = () => {
     const canvasRef = useRef(null);
 
+    const world = {
+        width: 1000,
+        height: 3000
+    };
+
     const player = {
         x: 400,
         y: 275,
@@ -11,11 +16,14 @@ const GameCanvas = () => {
         speed: 1
     };
 
+    const canvasWidth = 800;
+    const canvasHeight = 550;
+
     const pressedKeys = {};
 
     const drawPlayer = (ctx) => {
         ctx.fillStyle = '#f0f0f0';
-        ctx.fillRect(0, 0, 800, 550);
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
         ctx.beginPath();
         ctx.arc(player.x, player.y, player.size, 0, Math.PI * 2);
         ctx.fillStyle = player.color;
@@ -32,9 +40,9 @@ const GameCanvas = () => {
 
     const movePlayer = () => {
         if ((pressedKeys['ArrowUp'] || pressedKeys['w']) && player.y - player.size > 0) player.y -= player.speed;
-        if (pressedKeys['ArrowDown'] || pressedKeys['s'] && player.y + player.size < 550) player.y += player.speed;
-        if (pressedKeys['ArrowLeft'] || pressedKeys['a'] && player.x - player.size > 0) player.x -= player.speed;
-        if (pressedKeys['ArrowRight'] || pressedKeys['d'] && player.x + player.size < 800) player.x += player.speed;
+        if ((pressedKeys['ArrowDown'] || pressedKeys['s']) && player.y + player.size < world.height) player.y += player.speed;
+        if ((pressedKeys['ArrowLeft'] || pressedKeys['a']) && player.x - player.size > 0) player.x -= player.speed;
+        if ((pressedKeys['ArrowRight'] || pressedKeys['d']) && player.x + player.size < world.width) player.x += player.speed;
     };
 
     useEffect(() => {
@@ -43,7 +51,19 @@ const GameCanvas = () => {
 
         const gameLoop = () => {
             movePlayer();
+
+            context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+            const offsetX = player.x - canvasWidth / 2;
+            const offsetY = player.y - canvasHeight / 2;
+
+            context.save();
+            context.translate(-offsetX, -offsetY);
+
             drawPlayer(context);
+
+            context.restore();
+
             requestAnimationFrame(gameLoop);
         };
 
@@ -62,8 +82,8 @@ const GameCanvas = () => {
     return (
         <canvas
         ref={canvasRef}
-        width={800}
-        height={550}
+        width={canvasWidth}
+        height={canvasHeight}
         style={{ border: '1px solid black' }}
          />
     );
